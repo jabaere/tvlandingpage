58de063fd7c9b5cfb7ddb374309d67280a363dfe
jaba jaba <frosky.ge@gmail.com>
2023-09-21 19:49:13 +0400

    change answers

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,68 +1,67 @@
-
 export const faqData = [
-    {
-        question:'What is the collection size ?',
-        answer:'I will not tell you'
-    },
-    {
-        question:'When is the mint date ?',
-        answer:'I will not tell you'
-    },
-    {
-        question:'What is the Mint price ?',
-        answer:'I will not tell you'
-    },
-    {
-        question:'What are the benifits of holding the Cultpass ?',
-        answer:'I will not tell you'
-    },
-    {
-        question:'Is the team doxxed ?',
-        answer:'I will not tell you'
-    }
-]
+  {
+    question: "What is the collection size ?",
+    answer: `i̵ ̷w̵o̴u̶l̶d̴n̶'̵t̷ ̵t̷e̵l̴l̵ ̸y̷o̴u̸ ̴`,
+  },
+  {
+    question: "When is the mint date ?",
+    answer: `i̵ ̷w̵o̴u̶l̶d̴n̶'̵t̷ ̵t̷e̵l̴l̵ ̸y̷o̴u̸ ̴`,
+  },
+  {
+    question: "What is the Mint price ?",
+    answer: `i̵ ̷w̵o̴u̶l̶d̴n̶'̵t̷ ̵t̷e̵l̴l̵ ̸y̷o̴u̸ ̴`,
+  },
+  {
+    question: "What are the benifits of holding the Cultpass ?",
+    answer: `i̵ ̷w̵o̴u̶l̶d̴n̶'̵t̷ ̵t̷e̵l̴l̵ ̸y̷o̴u̸ ̴`,
+  },
+  {
+    question: "Is the team doxxed ?",
+    answer: `i̵ ̷w̵o̴u̶l̶d̴n̶'̵t̷ ̵t̷e̵l̴l̵ ̸y̷o̴u̸ ̴`,
+  },
+];
 
 export const galleryData = [
-      {
-        cover: "images/1.png",
-      },
-      {
-        cover: "images/2.png",
-      },
-      {
-        cover: "/images/3.png",
-      },
-      {
-        cover: "/images/4.png",
-      },
-      {
-        cover: "/images/5.png",
-      }
-]
+  {
+    cover: "images/1.png",
+  },
+  {
+    cover: "images/2.png",
+  },
+  {
+    cover: "/images/3.png",
+  },
+  {
+    cover: "/images/4.png",
+  },
+  {
+    cover: "/images/5.png",
+  },
+];
 
 export const teamData = [
-    {
-        picture:'images/team1.png',
-        name:'STEVE',
-        position:'Developer',
-        twitLink:'#'
-    },
-    {
-        picture:'images/team2.png',
-        name:'HORRISH',
-        position:'Project Manager',
-        twitLink:'#'
-    },
-    {
-        picture:'images/team3.png',
-        name:'DUKE',
-        position:'Content Creator',
-        twitLink:'#'
-    },
-    {
-        picture:'images/team4.png',
-        name:'JARED',
-        position:'Artist',
-        twitLink:'#'
-    }
-]
+  {
+    picture: "images/team1.png",
+    name: "STEVE",
+    position: "Developer",
+    twitLink: "#",
+  },
+  {
+    picture: "images/team2.png",
+    name: "HORRISH",
+    position: "Project Manager",
+    twitLink: "#",
+  },
+  {
+    picture: "images/team3.png",
+    name: "DUKE",
+    position: "Content Creator",
+    twitLink: "#",
+  },
+  {
+    picture: "images/team4.png",
+    name: "JARED",
+    position: "Artist",
+    twitLink: "#",
+  },
+];
